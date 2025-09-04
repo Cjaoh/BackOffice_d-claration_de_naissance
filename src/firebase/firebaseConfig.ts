ef949582c76fb07f1c6b6b14c0e88388f81d06b7
Snake <snake@localhost.localdomain>
2025-09-04 16:45:38 +0300

    update

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAtAMgS6Om-XRSROxu86PrnIV7XlXGYCkI",
   authDomain: "etatcivil-d7daa.firebaseapp.com",
   projectId: "etatcivil-d7daa",
-  storageBucket: "etatcivil-d7daa.firebasestorage.app",
+  storageBucket: "etatcivil-d7daa.appspot.com", // <-- Correction ici
   messagingSenderId: "95444634348",
   appId: "1:95444634348:web:404b92382ca07dfc0cf3c9",
   measurementId: "G-811JYW9VJK"
